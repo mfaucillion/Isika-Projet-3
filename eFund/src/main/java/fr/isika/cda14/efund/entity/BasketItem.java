cfddd6e24bb172c53ec23a09b850df0fb93d8000
nacer Benchehida <nacer.benchehida10@gmail.com>
2022-04-12 15:24:51 +0200

    Creation de la partie SHOP

---
+++ eFund/src/main/java/fr/isika/cda14/efund/entity/BasketItem.java
@@ -0,0 +1,35 @@
+package fr.isika.cda14.efund.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class BasketItem {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+
+	private String label;
+
+	@Column(name="quantity_stock")
+	private Integer quantityStock;
+
+	@Column(scale=2)
+	private Double price;
+
+	@Column
+	private String description;
+
+	@Enumerated(EnumType.STRING)
+	private  ItemStatus itemStatus;
+
+	@Enumerated(EnumType.STRING)
+	private ItemCategory itemCategory;
+
+}
