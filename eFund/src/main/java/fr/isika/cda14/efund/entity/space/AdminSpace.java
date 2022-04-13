9a38628a463f09f59cb4795b3197c66c352ea550
fatiifat <fatima.kaya@hotmail.com>
2022-04-13 12:23:44 +0200

    création adminSpace

---
+++ eFund/src/main/java/fr/isika/cda14/efund/entity/space/AdminSpace.java
@@ -0,0 +1,42 @@
+package fr.isika.cda14.efund.entity.space;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import fr.isika.cda14.efund.entity.account.OrganizationAccount;
+import fr.isika.cda14.efund.entity.account.UserAccount;
+import fr.isika.cda14.efund.entity.project.GenericProject;
+import fr.isika.cda14.efund.entity.shop.BasketOrder;
+
+@Entity
+@Table(name = "admin_space")
+public class AdminSpace {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@OneToMany
+	@JoinColumn(name = "admin_space_id")
+	private List<UserAccount> userAccounts;
+
+	@OneToMany
+	@JoinColumn(name = "admin_space_id")
+	private List<OrganizationAccount> organizationAccounts;
+
+	@OneToMany
+	@JoinColumn(name = "admin_space_id")
+	private List<BasketOrder> basketOrders;
+
+	@OneToMany
+	@JoinColumn(name = "admin_space_id")
+	private List<GenericProject> genericProjects;
+
+}
