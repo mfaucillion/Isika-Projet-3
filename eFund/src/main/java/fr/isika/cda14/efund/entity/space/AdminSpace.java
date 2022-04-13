b53da98e995746448ba114c56618f1996dcd355a
fatiifat <fatima.kaya@hotmail.com>
2022-04-13 12:28:44 +0200

    suite creation adminSpace

--- eFund/src/main/java/fr/isika/cda14/efund/entity/space/AdminSpace.java
+++ eFund/src/main/java/fr/isika/cda14/efund/entity/space/AdminSpace.java
@@ -8,8 +8,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import fr.isika.cda14.efund.entity.account.AdminAccount;
 import fr.isika.cda14.efund.entity.account.OrganizationAccount;
 import fr.isika.cda14.efund.entity.account.UserAccount;
 import fr.isika.cda14.efund.entity.project.GenericProject;
@@ -22,6 +24,10 @@ public class AdminSpace {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+
+	@OneToOne
+	@JoinColumn(name = "admin_account_id")
+	private AdminAccount adminAccount;
 
 	@OneToMany
 	@JoinColumn(name = "admin_space_id")
