b25393a56e5e3183768cbd0f82b5047661af328e
HelyZika <sr.rchouk@gmail.com>
2022-04-14 11:28:04 +0200

    payment MVC

---
+++ eFund/src/main/java/fr/isika/cda14/efund/managedbeans/CreatePaymentBean.java
@@ -0,0 +1,36 @@
+package fr.isika.cda14.efund.managedbeans;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+
+import fr.isika.cda14.efund.services.PaymentService;
+import fr.isika.cda14.efund.viewModel.PaymentForm;
+
+@ManagedBean
+@ViewScoped
+public class CreatePaymentBean implements Serializable {
+	private static final long serialVersionUID = 0;
+
+	@Inject
+	private PaymentService paymentService;
+
+	private PaymentForm paymentForm = new PaymentForm();
+
+	
+	public void createInvoice() {
+		System.out.println("Service : " + paymentForm);
+		;
+	}
+
+	public PaymentForm getPaymentForm() {
+		return paymentForm;
+	}
+
+	public void setPaymentForm(PaymentForm paymentForm) {
+		this.paymentForm = paymentForm;
+	}
+
+}
