31956b9415c2ad7130a2b8bae4382c26bd6c14bc
mfaucillion <matthieu.faucillion@gmail.com>
2022-04-15 02:50:13 +0200

    Primefaces datepicker implemented

--- eFund/src/main/java/fr/isika/cda14/efund/viewmodel/CreateUserBisViewModel.java
+++ eFund/src/main/java/fr/isika/cda14/efund/viewmodel/CreateUserBisViewModel.java
@@ -2,116 +2,84 @@ package fr.isika.cda14.efund.viewmodel;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 public class CreateUserBisViewModel implements Serializable {
 
-		private static final long serialVersionUID = -7701014377447371993L;
+	private static final long serialVersionUID = -7701014377447371993L;
 
-		@NotEmpty(message = "Ne doit pas être vide")
-		@NotNull(message = "Ne doit pas être null")
-		@Size(min = 1, max = 25, message = "Doit être entre 1 et 25 car.")
-		private String lastName;
+	private String lastName;
 
-		@NotEmpty(message = "Ne doit pas être vide")
-		@NotNull(message = "Ne doit pas être null")
-		@Size(min = 1, max = 25, message = "Doit être entre 1 et 25 car.")
-		private String firstName;
+	private String firstName;
 
-		@NotEmpty(message = "Ne doit pas être vide")
-		@NotNull(message = "Ne doit pas être null")
-		@Size(min = 1, max = 25, message = "Doit être entre 1 et 25 car.")
-		private String phone;
-		
+	private String phone;
 
-		@NotEmpty(message = "Ne doit pas être vide")
-		@NotNull(message = "Ne doit pas être null")
-		@Size(min = 1, max = 50, message = "Doit être entre 1 et 50 car.")
-		private String address;
-		
-		@NotEmpty(message = "Ne doit pas être vide")
-		@NotNull(message = "Ne doit pas être null")
-		@Size(min = 1, max = 50, message = "Doit être entre 1 et 50 car.")
-		private String zipcode;
-		
-		@NotEmpty(message = "Ne doit pas être vide")
-		@NotNull(message = "Ne doit pas être null")
-		@Size(min = 1, max = 50, message = "Doit être entre 1 et 50 car.")
-		private String city;
-		
-		@NotEmpty(message = "Ne doit pas être vide")
-		@NotNull(message = "Ne doit pas être null")
-		@Size(min = 1, max = 50, message = "Doit être entre 1 et 50 car.")
-		private String country;
+	private String address;
 
-		public String getLastName() {
-			return lastName;
-		}
+	private String zipcode;
 
-		public void setLastName(String lastName) {
-			this.lastName = lastName;
-		}
+	private String city;
 
-		public String getFirstName() {
-			return firstName;
-		}
+	private String country;
 
-		public void setFirstName(String firstName) {
-			this.firstName = firstName;
-		}
+	public String getLastName() {
+		return lastName;
+	}
 
-		public String getPhone() {
-			return phone;
-		}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-		public void setPhone(String phone) {
-			this.phone = phone;
-		}
+	public String getFirstName() {
+		return firstName;
+	}
 
-		public String getAddress() {
-			return address;
-		}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-		public void setAddress(String address) {
-			this.address = address;
-		}
+	public String getPhone() {
+		return phone;
+	}
 
-		public String getZipcode() {
-			return zipcode;
-		}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-		public void setZipcode(String zipcode) {
-			this.zipcode = zipcode;
-		}
+	public String getAddress() {
+		return address;
+	}
 
-		public String getCity() {
-			return city;
-		}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-		public void setCity(String city) {
-			this.city = city;
-		}
+	public String getZipcode() {
+		return zipcode;
+	}
 
-		public String getCountry() {
-			return country;
-		}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
 
-		public void setCountry(String country) {
-			this.country = country;
-		}
+	public String getCity() {
+		return city;
+	}
 
-		@Override
-		public String toString() {
-			return "CreateUserBisViewModel [lastName=" + lastName + ", firstName=" + firstName + ", phone=" + phone
-					+ ", address=" + address + ", zipcode=" + zipcode + ", city=" + city + ", country=" + country + "]";
-		}
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-	
+	public String getCountry() {
+		return country;
+	}
 
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
-	
-	
-}	
-	
+	@Override
+	public String toString() {
+		return "CreateUserBisViewModel [lastName=" + lastName + ", firstName=" + firstName + ", phone=" + phone
+				+ ", address=" + address + ", zipcode=" + zipcode + ", city=" + city + ", country=" + country + "]";
+	}
+
+}
--- eFund/src/main/java/fr/isika/cda14/efund/viewmodel/CreateUserViewModel.java
+++ eFund/src/main/java/fr/isika/cda14/efund/viewmodel/CreateUserViewModel.java
@@ -2,30 +2,14 @@ package fr.isika.cda14.efund.viewmodel;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
-
-
 public class CreateUserViewModel implements Serializable {
 
 	private static final long serialVersionUID = -7701014377447371993L;
 
-	@NotEmpty(message = "Ne doit pas être vide")
-	@NotNull(message = "Ne doit pas être null")
-	@Email
 	private String email;
 
-	@NotEmpty(message = "Ne doit pas être vide")
-	@NotNull(message = "Ne doit pas être null")
-	@Size(min = 1, max = 25, message = "Doit être entre 1 et 25 car.")
 	private String password;
 
-	@NotEmpty(message = "Ne doit pas être vide")
-	@NotNull(message = "Ne doit pas être null")
-	@Size(min = 1, max = 25, message = "Doit être entre 1 et 25 car.")
 	private String displayedName;
 
 	public String getEmail() {
