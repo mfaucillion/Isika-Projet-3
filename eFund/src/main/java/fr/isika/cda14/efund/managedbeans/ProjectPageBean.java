c3bcbda84df4f8b5e689a1e734994d861813a60a
stephqui <paradivuiz@gmail.com>
2022-04-15 14:56:36 +0200

    embryon de page projet + son bean associé

---
+++ eFund/src/main/java/fr/isika/cda14/efund/managedbeans/ProjectPageBean.java
@@ -0,0 +1,18 @@
+
+package fr.isika.cda14.efund.managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
+
+import fr.isika.cda14.efund.services.ProjectService;
+import fr.isika.cda14.efund.viewmodel.ProjectCreationFormVM;
+
+@ManagedBean
+public class ProjectPageBean {
+
+	@Inject
+	private ProjectService projectCreationService;
+
+	private ProjectCreationFormVM projectCreationFormVM = new ProjectCreationFormVM();
+
+}
