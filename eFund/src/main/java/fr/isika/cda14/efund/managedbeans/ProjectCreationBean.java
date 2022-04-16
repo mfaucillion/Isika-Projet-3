a6d002cf8a7f0981e647408e9ef71e7d68ed5ad5
stephqui <paradivuiz@gmail.com>
2022-04-16 19:56:16 +0200

    mise en forme page creation projet

--- eFund/src/main/java/fr/isika/cda14/efund/managedbeans/ProjectCreationBean.java
+++ eFund/src/main/java/fr/isika/cda14/efund/managedbeans/ProjectCreationBean.java
@@ -1,12 +1,14 @@
 package fr.isika.cda14.efund.managedbeans;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
 import fr.isika.cda14.efund.services.ProjectService;
 import fr.isika.cda14.efund.viewmodel.ProjectCreationFormVM;
 
 @ManagedBean
+@ViewScoped
 public class ProjectCreationBean {
 
 	@Inject
