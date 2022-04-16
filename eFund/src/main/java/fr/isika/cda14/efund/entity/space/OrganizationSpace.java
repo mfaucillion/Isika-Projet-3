81373349fe446ef03c775c006afd90eaa0e3a08e
Renaud <renaudtoyer@gmail.com>
2022-04-16 02:44:10 +0200

    Structure de la page d'organisation terminée, début de lien avec les autres pages et la base de données

--- eFund/src/main/java/fr/isika/cda14/efund/entity/space/OrganizationSpace.java
+++ eFund/src/main/java/fr/isika/cda14/efund/entity/space/OrganizationSpace.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -35,7 +36,7 @@ public class OrganizationSpace {
 	@JoinColumn(name = "organization_space_id")
 	private List<ContentTab> contentTabs;
 
-	@OneToMany
+	@OneToMany(fetch = FetchType.EAGER)
 	@JoinColumn(name = "organization_space_id")
 	private List<GenericProject> genericProjects;
 
