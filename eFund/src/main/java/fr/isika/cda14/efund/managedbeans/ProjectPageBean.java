e628e7ad96d9a486e2a54b160b267eeba899c817
stephqui <paradivuiz@gmail.com>
2022-04-20 12:34:19 +0200

    calcul date restante: erreur de parseDate

--- eFund/src/main/java/fr/isika/cda14/efund/managedbeans/ProjectPageBean.java
+++ eFund/src/main/java/fr/isika/cda14/efund/managedbeans/ProjectPageBean.java
@@ -2,8 +2,13 @@
 package fr.isika.cda14.efund.managedbeans;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
 import fr.isika.cda14.efund.entity.account.OrganizationAccount;
@@ -37,7 +42,23 @@ public class ProjectPageBean {
 	}
 	
 	public int percentage(BigDecimal currentCollect, BigDecimal target) {
-		return (currentCollect.intValue() * 100)/target.intValue();
+		return (currentCollect.intValue() * 100)/target.intValue();		
+	}
+	
+	public int calculRemainingDays(String endDate) {
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+	     try {
+	    	 LocalDate todaysDate = LocalDate.now();
+	    	 System.out.println("ce jour : " + todaysDate + " test date : " + endDate);
+	         Date dateAvant = sdf.parse("02/25/2021");
+	         Date dateApres = sdf.parse(endDate);
+	         long diff = dateApres.getTime() - dateAvant.getTime();
+	         float res = (diff / (1000*60*60*24));
+	         System.out.println("Nombre de jours entre les deux dates est: "+ res + "date du jour : " + todaysDate);
+	     } catch (Exception e) {
+	         e.printStackTrace();
+	     }
+        return 123;
 		
 	}
 }
