19ff81f92c92aa2246bca83cbf2224d2614f4c97
stephqui <paradivuiz@gmail.com>
2022-04-25 16:29:30 +0200

    creation squelette modif Org form

--- eFund/src/main/java/fr/isika/cda14/efund/managedbeans/OrgModificationProfileBean.java
+++ eFund/src/main/java/fr/isika/cda14/efund/managedbeans/OrgModificationProfileBean.java
@@ -1,30 +1,21 @@
 package fr.isika.cda14.efund.managedbeans;
 
-import java.util.List;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
 import fr.isika.cda14.efund.entity.account.OrganizationAccount;
-import fr.isika.cda14.efund.entity.account.UserAccount;
-import fr.isika.cda14.efund.entity.project.Event;
-import fr.isika.cda14.efund.entity.project.Project;
 import fr.isika.cda14.efund.services.AccountService;
 
 @ViewScoped
 @ManagedBean
-public class OngProfileBean {
-	
+public class OrgModificationProfileBean {
+
 	@Inject
 	private AccountService accService;
-	
+
 	private OrganizationAccount organizationAccount;
-	
-	private List<Project> projects;
-	
-	private List<Event> events;
-	
+
 	public void onLoad(String id) {
 		this.organizationAccount = accService.findOrganizationAccount(Long.parseLong(id));
 	}
