55b025f7382bb1fe0f76389c28139c8ef69415ec
Renaud <renaudtoyer@gmail.com>
2022-04-25 04:23:57 +0200

    Added ImgPath and config property + refactored a bit

--- eFund/src/main/java/fr/isika/cda14/efund/entity/space/OrganizationSpace.java
+++ eFund/src/main/java/fr/isika/cda14/efund/entity/space/OrganizationSpace.java
@@ -1,8 +1,10 @@
 package fr.isika.cda14.efund.entity.space;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,16 +21,35 @@ import fr.isika.cda14.efund.entity.shop.Shop;
 
 @Entity
 @Table(name = "organization_space")
-public class OrganizationSpace {
+public class OrganizationSpace implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5730766511514977861L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
+	private String imagePath;
+	
+	@Column(name = "presentation_tab")
+	private Boolean mainTab;
+	
+	@Column(name = "shop_tab")
+	private Boolean shopTab;
+	
+	@Column(name = "projects_tab")
+	private Boolean projectsTab;
+	
+	@Column(name = "events_tab")
+	private Boolean eventsTab;
+	
 	@OneToOne(cascade = CascadeType.ALL)
 	private Shop shop;
 
-	@OneToMany
+	@OneToMany(cascade = CascadeType.ALL)
 	@JoinColumn(name = "organization_space_id")
 	private List<Follow> follows;
 
@@ -42,6 +63,11 @@ public class OrganizationSpace {
 	@OneToMany(mappedBy = "organizationSpace", cascade = CascadeType.ALL)
 	private List<Event> events;
 	
+	public OrganizationSpace() {
+		this.shop = new Shop();
+		this.imagePath = "/img/space/default.jpg";
+	}
+	
 	public Shop getShop() {
 		return shop;
 	}
@@ -54,36 +80,61 @@ public class OrganizationSpace {
 		return follows;
 	}
 
-	public void setFollows(List<Follow> follows) {
-		this.follows = follows;
-	}
-
 	public List<ContentBlock> getContentBlocks() {
 		return contentBlocks;
-	}
-
-	public void setContentBlocks(List<ContentBlock> contentBlocks) {
-		this.contentBlocks = contentBlocks;
 	}
 
 	public List<Project> getProjects() {
 		return projects;
 	}
 
-	public void setProjects(List<Project> projects) {
-		this.projects = projects;
-	}
-
 	public List<Event> getEvents() {
 		return events;
-	}
-
-	public void setEvents(List<Event> events) {
-		this.events = events;
 	}
 
 	public Long getId() {
 		return id;
 	}
+
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+	public Boolean getMainTab() {
+		return mainTab;
+	}
+
+	public void setMainTab(Boolean mainTab) {
+		this.mainTab = mainTab;
+	}
+
+	public Boolean getShopTab() {
+		return shopTab;
+	}
+
+	public void setShopTab(Boolean shopTab) {
+		this.shopTab = shopTab;
+	}
+
+	public Boolean getProjectsTab() {
+		return projectsTab;
+	}
+
+	public void setProjectsTab(Boolean projectsTab) {
+		this.projectsTab = projectsTab;
+	}
+
+	public Boolean getEventsTab() {
+		return eventsTab;
+	}
+
+	public void setEventsTab(Boolean eventsTab) {
+		this.eventsTab = eventsTab;
+	}
+	
 
 }
